427de8b2cf5639f97b3a49fc4e3e4d0ab8b1d757
pickittwice <s4miko@163.com>
2022-12-11 23:15:30 +0800

    fix bug
    Change-Id: I028b4673481eb350815200f444da8b10cb086721

--- init_tmux.sh
+++ init_tmux.sh
@@ -3,6 +3,6 @@
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 cp .tmux.conf ~/.tmux.conf
 tmux start-server
-tmux source-file ~/.tumx.conf
+tmux source-file ~/.tmux.conf
 # install the plugins
 ~/.tmux/plugins/tpm/scripts/install_plugins.sh
