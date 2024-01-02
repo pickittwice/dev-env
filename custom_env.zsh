5bd7cd606558ddf23d252ab786ceac4561896612
pickittwice <16740865+pickittwice@users.noreply.github.com>
2024-01-02 15:25:06 +0800

    cd_up multi-platform compatible
    Change-Id: I0165fe18ac09c94a6946a381250e90996eef925b

--- custom_env.zsh
+++ custom_env.zsh
@@ -4,7 +4,7 @@ alias du1="du -hd 1 2>/dev/null | sort -hr"
 alias adl="adb logcat"
 
 function cd_up() {
-  cd $(printf "%0.s../" $(seq 1 $1 ));
+  cd $(printf "%0.s../" $(1..${1}));
 }
 
 alias 'cd..'='cd_up'
